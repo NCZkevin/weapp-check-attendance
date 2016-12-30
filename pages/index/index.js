7226f1dc59ad42b14606d1b31e506dce64e82d92
nczkevin <z1376871863@163.com>
2016-12-30 16:52:29 +0800

    box shadow

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,9 @@ Page({
       { title: '计算机网络', teacher: '王小明', address: '软件楼121', time: '周一第三节课', status: '0' },
       { title: '计算机网络', teacher: '王小明', address: '软件楼121', time: '周一第三节课', status: '1' },
       { title: '计算机网络', teacher: '王小明', address: '软件楼121', time: '周一第三节课', status: '2' },
+      { title: '计算机网络', teacher: '王小明', address: '软件楼121', time: '周一第三节课', status: '2' },
+      { title: '计算机网络', teacher: '王小明', address: '软件楼121', time: '周一第三节课', status: '2' },
+      { title: '计算机网络', teacher: '王小明', address: '软件楼121', time: '周一第三节课', status: '2' },
     ]
   }
 })
