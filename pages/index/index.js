298f17d5e45e15f2f03990e5936aeb050ddf7ee5
nczkevin <z1376871863@163.com>
2016-12-30 16:43:09 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,10 @@
 Page({
   data: {
     text: 'init data',
-    array: [{msg: '1'}, {msg: '2'}]
+    core: [
+      { title: '计算机网络', teacher: '王小明', address: '软件楼121', time: '周一第三节课', status: '0' },
+      { title: '计算机网络', teacher: '王小明', address: '软件楼121', time: '周一第三节课', status: '1' },
+      { title: '计算机网络', teacher: '王小明', address: '软件楼121', time: '周一第三节课', status: '2' },
+    ]
   }
 })
